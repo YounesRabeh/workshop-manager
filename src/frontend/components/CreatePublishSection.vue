2c50c588dbf0c3aa4b970befc0e79affcb051be6
YounesRabeh <rabehyounes77@gmail.com>
2026-03-19 14:39:02 +0100

    [IMP] publish area

---
+++ src/frontend/components/CreatePublishSection.vue
@@ -0,0 +1,12 @@
+<script lang="ts">
+import { defineComponent, h } from 'vue'
+import CreatePublishSectionImpl from './publish/CreatePublishSection.vue'
+
+export default defineComponent({
+  name: 'CreatePublishSectionCompat',
+  inheritAttrs: false,
+  setup(_, { attrs, slots }) {
+    return () => h(CreatePublishSectionImpl as any, attrs, slots)
+  }
+})
+</script>
---
+++ src/frontend/components/UpdatePublishSection.vue
@@ -0,0 +1,12 @@
+<script lang="ts">
+import { defineComponent, h } from 'vue'
+import UpdatePublishSectionImpl from './publish/UpdatePublishSection.vue'
+
+export default defineComponent({
+  name: 'UpdatePublishSectionCompat',
+  inheritAttrs: false,
+  setup(_, { attrs, slots }) {
+    return () => h(UpdatePublishSectionImpl as any, attrs, slots)
+  }
+})
+</script>
